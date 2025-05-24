179c4ab4a677a4e78a509a3f992609c67d26bfbf
cathiecode <cathiecode@gmail.com>
2025-05-24 18:35:33 +0900

    test: sprite tests

--- src/sprite.rs
+++ src/sprite.rs
@@ -15,7 +15,7 @@ struct Sprite {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct SpriteSheetMeta {
     sprites: HashMap<String, Sprite>,
-    sheet: String,
+    image: String,
 }
 
 #[derive(Debug, Clone)]
@@ -26,12 +26,16 @@ pub struct SpriteSheet {
 
 impl SpriteSheet {
     pub fn load(path: &str) -> Result<Self, String> {
-        let file = std::fs::File::open(path).map_err(|e| e.to_string())?;
+        let sheet_path = PathBuf::from(path);
+        let file = std::fs::File::open(&sheet_path).map_err(|e| e.to_string())?;
         let sprite_sheet_meta: SpriteSheetMeta =
             serde_json::from_reader(file).map_err(|e| e.to_string())?;
-        let sheet_path: PathBuf = PathBuf::from(path.to_string() + "/" + &sprite_sheet_meta.sheet);
+        
+        let image_path: PathBuf = sheet_path.parent().unwrap().join(sprite_sheet_meta.image.clone());
 
-        let pixmap = Pixmap::load_png(sheet_path).map_err(|e| e.to_string())?;
+        log::info!("Loading sprite sheet: {}", image_path.display());
+
+        let pixmap = Pixmap::load_png(image_path.clone()).map_err(|e| format!("{}: {}", e.to_string(), image_path.display()))?;
 
         Ok(Self {
             meta: sprite_sheet_meta,
@@ -51,5 +55,34 @@ impl SpriteSheet {
         };
 
         self.pixmap.clone_rect(rect)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn load_test_sprite_sheet() -> SpriteSheet {
+        SpriteSheet::load("test_files/sprites/sprites.json").unwrap()
+    }
+
+    #[test]
+    fn test_load_sprite_sheet() {
+        SpriteSheet::load("test_files/sprites/sprites.json").unwrap();
+    }
+
+    #[test]
+    fn test_cutout_sprite() {
+        let sprite_sheet = load_test_sprite_sheet();
+
+        let sprite_s = sprite_sheet.cutout("s");
+        assert!(sprite_s.is_some());
+
+        assert_eq!(Pixmap::load_png("test_files/sprites/s.png").unwrap(), sprite_s.unwrap());
+
+        let sprite_p = sprite_sheet.cutout("p");
+        assert!(sprite_p.is_some());
+
+        assert_ne!(Pixmap::load_png("test_files/sprites/s.png").unwrap(), sprite_p.unwrap());
     }
 }
